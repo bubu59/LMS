a10b1a63e953208a4bb666020e5104b0aed8eb2f
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2023-09-19 17:32:39 +0800

    chore: dashboard layout

---
+++ app/(dashboard)/_components/Logo.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <Image
+        height={100}
+        width={100}
+        alt='logo'
+        src='/logo.svg'
+    />
+  )
+} 
+
+export default Logo
